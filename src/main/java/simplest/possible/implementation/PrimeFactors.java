d0c354c646ca9f3b1ea0a060957ec86b5166fd8b
Hyndhavi Muktineni <hyndhavi1999@gmail.com>
2021-01-09 17:09:45 +0530

    Added class PrimeFactors

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,11 @@
+package simplest.possible.implementation;
+
+import java.util.Collections;
+import java.util.List;
+
+public class PrimeFactors {
+    public List<Integer> of(int number)
+    {
+        return Collections.emptyList();
+    }
+}
